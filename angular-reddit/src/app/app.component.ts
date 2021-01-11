ddd77735d4e03cc8ec4cdd3e21ba22a7e29b905b
palo-landrae <palo.loui.andrae@iisgalvanimi.edu.it>
2021-01-11 08:32:33 +0000

    commit 2

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reddit';
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    let t = title.value;
+    let v= link.value;
+    console.log(`Adding article title: `+t+ ` and link: `+ v);
+    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  return false;
+  }
 }
